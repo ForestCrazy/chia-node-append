22243f4d44f44068429ba40087834e61df084f04
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-05-18 12:50:27 +0700

    fix object key

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ const { homedir } = require('os');
 const { join } = require('path');
 const lodash = require('lodash');
 const firebase = require('firebase');
+const winston = require('winston');
+const { format } = require('logform');
+const { combine, timestamp, label, printf } = format;
 
 (async() => {
-    process.on('uncaughtException', function(err) {
-        console.log(err);
+    const logFormat = printf(({ level, message, timestamp }) => {
+        return `${timestamp} ${level}: ${message}`;
     });
+    var logger = winston.createLogger({
+        level: 'info',
+        format: combine(
+            timestamp(),
+            logFormat
+        ),
+        transports: [
+            new winston.transports.Console()
+        ]
+    });
+    process.on('uncaughtException', function(err) {
+        logger.error('Error: ' + err);
+    });
+    logger.info('connection to chia service');
     const conn = new Connection('localhost:55400', {
         cert: readFileSync(join(homedir(), '.chia', 'mainnet', 'config/ssl/daemon/private_daemon.crt')),
         key: readFileSync(join(homedir(), '.chia', 'mainnet', 'config/ssl/daemon/private_daemon.key')),
     });
+    logger.info('Initialize FullNode ApiClient');
     const fullNode = new ApiClient.FullNode({ connection: conn, origin: 'chia-node-append' });
     await fullNode.init();
 
+    logger.info('Initialize Firebase App');
     const app = firebase.initializeApp({
         apiKey: "AIzaSyC-QfMwDfqD8C-Jsu0K-RsBFB1gZMfhphY", // Auth / General Use
         appId: "1:279516045079:web:1cf491eeefd5d709356152", // General Use
@@ -28,11 +47,11 @@ const firebase = require('firebase');
         var resource_node_list = null;
         const node_list_file = 'node_list.txt';
         if (existsSync(node_list_file)) {
+            logger.info('import node list from node_list.txt');
             resource_node_list = 'node_list_file';
-            const node_arr = readFileSync(node_list_file, function(err, data) {
+            var node_obj = readFileSync(node_list_file, function(err, data) {
                 if (err) throw err;
             }).toString().split('\n');
-            var node_obj = Object.assign({}, node_arr);
             for (const property in node_obj) {
                 node_obj[property] = {
                     node_ip: node_obj[property].split(':')[0],
@@ -40,56 +59,60 @@ const firebase = require('firebase');
                 }
             }
         } else {
+            logger.info('import node list from firestore database');
             resource_node_list = 'node_list_firebase';
             if (while_loop_round % 10 == 0) {
-                const firestore_node_list = await app.firestore().collection('node_list').get()
+                const firestore_node_list = await app.firestore().collection('node_list').get();
                 var node_obj = firestore_node_list.docs.map(doc => doc.data());
             }
         }
 
         const currConnections = await fullNode.getConnections();
-        const currConn_obj = JSON.parse(JSON.stringify(currConnections));
+        const currConn_obj = JSON.parse(JSON.stringify(currConnections))['connections'];
 
-        for (const property in node_obj) {
-            const node_chia = lodash.filter(currConn_obj['connections'], obj_item => obj_item.peer_host === node_obj[property].node_ip);
-            if (Object.keys(node_chia).length == 0) {
-                console.log(node_obj[property])
-                try {
-                    const addNodeConnection = await fullNode.addNodeConnection({
-                        host: node_obj[property].node_ip,
-                        port: node_obj[property].node_port
-                    });
-                } catch (exception) {
-                    console.log(exception);
-                }
+        const node_chia = node_obj.filter(({ node_ip: node_1 }) => !currConn_obj.some(({ peer_host: node_2 }) => node_2 === node_1));
+        for (const property in node_chia) {
+            logger.info('add node connection ip: ' + node_chia[property].node_ip + ' port: ' + node_chia[property].node_port);
+            try {
+                const addNodeConnection = await fullNode.addNodeConnection({
+                    host: node_chia[property].node_ip,
+                    port: node_chia[property].node_port
+                });
+            } catch (exception) {
+                logger.error('failed to add node connection : ' + exception);
             }
         }
 
-        const filter_node_conn = lodash.filter(JSON.parse(JSON.stringify(await fullNode.getConnections())), obj_item => obj_item.type !== 1);
+        const filter_node_conn = lodash.filter(JSON.parse(JSON.stringify(await fullNode.getConnections()))['connections'], function(node_type) { return node_type.type != 1 });
 
         if (Object.keys(filter_node_conn).length > 0) {
             for (const property in filter_node_conn) {
                 if (filter_node_conn[property].peer_host !== '127.0.0.1') {
                     try {
+                        logger.info('close connection node with node_id : ' + filter_node_conn[property].node_id)
                         const closeNodeConnection = await fullNode.closeNodeConnection({
                             node_id: filter_node_conn[property].node_id
                         });
                     } catch (exception) {
-                        console.log(exception);
+                        logger.error('failed to close connection node : ' + exception);
                     }
                 }
             }
         }
 
         if (while_loop_round % 100 == 0 && while_loop_round !== 0) {
-            if (resource_node_list == 'node_list_firebase') {
-                const currConnections = lodash.filter(JSON.parse(JSON.stringify(await fullNode.getConnections())['connections']), obj_item => obj_item.type === 1);
-                for (const property in currConnections) {
-                    const newChiaNode = await app.firestore().collection('node_list').doc().set({
-                        node_ip: currConnections[property].peer_host,
-                        node_port: currConnections[property].peer_server_port
-                    });
-                }
+            if (resource_node_list !== 'node_list_firebase') {
+                var node_obj = await app.firestore().collection('node_list').get();
+                node_obj = node_obj.docs.map(doc => doc.data());
+            }
+            const currConnections = lodash.filter(JSON.parse(JSON.stringify(await fullNode.getConnections()))['connections'], obj_item => obj_item.type === 1);
+            const filter_node_list = currConnections.filter(({ peer_host: node_1 }) => !node_obj.some(({ node_ip: node_2 }) => node_2 === node_1));
+            for (const property in filter_node_list) {
+                logger.info('insert node ip: ' + currConnections[property].peer_host + ' port: ' + currConnections[property].peer_server_port);
+                const newChiaNode = await app.firestore().collection('node_list').doc().set({
+                    node_ip: currConnections[property].peer_host,
+                    node_port: currConnections[property].peer_server_port
+                });
             }
         }
         while_loop_round += 1;
