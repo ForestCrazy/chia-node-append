4b56fee2dfbaa453ac9aeac519d6e99bc8e49e1e
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-05-23 03:10:48 +0700

    change base endpoint node list api

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ const request = require('request');
         currConnections = lodash.filter(JSON.parse(JSON.stringify(await fullNode.getConnections()))['connections'], obj_item => obj_item.type === 1);
         for (const property in currConnections) {
             logger.info('active node ip: ' + currConnections[property].peer_host + ' port: ' + currConnections[property].peer_server_port + ' to node list api');
-            const activeNode = await Curl('http://localhost/node', 'PUT', {
+            const activeNode = await Curl('https://chia-node-list-api.vercel.app/node', 'PUT', {
                 node_ip: currConnections[property].peer_host,
                 node_port: currConnections[property].peer_server_port
             });
